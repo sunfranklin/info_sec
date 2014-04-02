717e0daf3fe2364abed1e7fe89f68e4167e98763
sunfranklin <sunfranklin@gmail.com>
2014-04-02 19:04:51 -0400

    Made some small UI changes, etc.

--- threats_and_preventions/src/com/example/threats_and_preventions/QuizActivity.java
+++ threats_and_preventions/src/com/example/threats_and_preventions/QuizActivity.java
@@ -7,6 +7,7 @@ import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.Window;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
@@ -32,6 +33,7 @@ public class QuizActivity extends Activity implements OnClickListener {
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);        
         setContentView(R.layout.activity_quiz);
 
         dbquiz.deleteTable();
